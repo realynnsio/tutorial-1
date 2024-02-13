e27f1e48bbb977902532af25ff4199beba616a44
jep <n.siomusic@gmail.com>
2024-02-13 20:01:16 +0700

    add ProductServiceImplTest.java

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,12 +2,20 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class EshopApplicationTests {
+
+	@Autowired
+	private EshopApplication eshopApplication;
 
 	@Test
 	void contextLoads() {
+		// Test to ensure the Spring context loads
+		assertNotNull(eshopApplication);
 	}
 
 }
